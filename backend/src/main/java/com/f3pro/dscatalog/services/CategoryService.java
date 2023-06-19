1942ed59acbb95a56e4cc15f4f549b90aee96d0c
Francivaldo Alves <francivaldo.alves@gmail.com>
2023-06-19 10:47:01 -0300

    find Category by id, exception handling

--- backend/src/main/java/com/f3pro/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/f3pro/dscatalog/services/CategoryService.java
@@ -2,13 +2,14 @@ package com.f3pro.dscatalog.services;
 
 import com.f3pro.dscatalog.dto.CategoryDTO;
 import com.f3pro.dscatalog.entities.Category;
+import com.f3pro.dscatalog.exceptions.ResourceNotFoundException;
 import com.f3pro.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -21,5 +22,11 @@ public class CategoryService {
         List<Category> list =repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
 
+    }
+    @Transactional(readOnly=true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        var entity =obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
     }
 }
