e9bc0b16b57af1cc5553d33b59c8e73358fd534f
Francivaldo Alves <francivaldo.alves@gmail.com>
2023-07-02 11:00:33 -0300

    add tests save, delete

---
+++ backend/src/test/java/com/f3pro/dscatalog/tests/Factory.java
@@ -0,0 +1,25 @@
+package com.f3pro.dscatalog.tests;
+
+import com.f3pro.dscatalog.dto.ProductDTO;
+import com.f3pro.dscatalog.entities.Category;
+import com.f3pro.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Goog phone", 800.0, "http://img.teste",
+                Instant.parse("2021-03-14T10:00:00Z"));
+        product.getCategories().add(createCategory());
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+    public static Category createCategory(){
+        return new Category(1L,"Eletronics");
+    }
+}
